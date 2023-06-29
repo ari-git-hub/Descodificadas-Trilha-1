b89817c247bf7e16ad317c68f68b627728ca7f90
AriCris <137232584+ari-git-hub@users.noreply.github.com>
2023-06-28 22:02:16 -0300

    Add files via upload

---
+++ Site completo/codigo.js
@@ -0,0 +1,13 @@
+alert ( "Feliz aniversário, Gael!!" )
+var data = new Date();
+var hora = data.getHours();
+// alert ("Hora:" +hora)
+if (1 <= hora <= 12) 
+    { alert ( "Bom dia!");
+} else if ( 13 <= hora <= 18) {
+    alert ( "Boa tarde!");
+} else {
+    alert ( "Boa noite!" );
+}   
+    
+    
